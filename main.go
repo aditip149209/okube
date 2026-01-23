7059a39eae3a6ef87ed7e4964904454a4fbb75f3
Aditi Pandey <aditip149209@gmail.com>
2026-01-23 14:42:22 +0530

    manager api

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"time"
 
+	"github.com/aditip149209/okube/pkg/manager"
 	"github.com/aditip149209/okube/pkg/task"
 	"github.com/aditip149209/okube/pkg/worker"
 	"github.com/golang-collections/collections/queue"
@@ -44,6 +45,39 @@ func main() {
 	go runTasks(&w)
 	go w.CollectStats()
 	api.Start()
+
+	workers := []string{fmt.Sprintf("%s:%d", host, port)}
+	m := manager.New(workers)
+
+	for i := 0; i < 3; i++ {
+		t := task.Task{
+			ID:    uuid.New(),
+			Name:  fmt.Sprintf("test-container-%d", i),
+			State: task.Scheduled,
+			Image: "strm/helloworld-http",
+		}
+		te := task.TaskEvent{
+			ID:    uuid.New(),
+			State: task.Running,
+			Task:  t,
+		}
+		m.AddTask(te)
+		m.SendWork()
+	}
+
+	go func() {
+		for {
+			m.UpdateTasks()
+			time.Sleep(15 * time.Second)
+		}
+	}()
+
+	for {
+		for _, t := range m.TaskDB {
+			fmt.Printf("[Manager] Task: id: %s, state: %d\n", t.ID, t.State)
+		}
+		time.Sleep(10 * time.Second)
+	}
 
 	// db := make(map[uuid.UUID]*task.Task)
 	// w1 := worker.Worker{
--- pkg/manager/manager.go
+++ pkg/manager/manager.go
@@ -6,9 +6,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/aditip149209/okube/pkg/task"
 	"github.com/aditip149209/okube/pkg/worker"
+	"github.com/go-chi/chi"
 	"github.com/golang-collections/collections/queue"
 	"github.com/google/uuid"
 )
@@ -148,8 +150,8 @@ func New(workers []string) *Manager {
 	WorkerTaskMap := make(map[string][]uuid.UUID)
 	TaskWorkerMap := make(map[uuid.UUID]string)
 
-	for worker := range Workers {
-		WorkerTaskMap[Workers[worker]] = []uuid.UUID{}
+	for worker := range workers {
+		WorkerTaskMap[workers[worker]] = []uuid.UUID{}
 	}
 
 	return &Manager{
@@ -160,5 +162,107 @@ func New(workers []string) *Manager {
 		WorkersTaskMap: WorkerTaskMap,
 		TaskWorkersMap: TaskWorkerMap,
 	}
+}
+
+// manager api- this is what allows the users to interact with the okube cluster. essentially, we are building a way for the end user to interact with okube
+type Api struct {
+	Address string
+	Port    int
+	Manager *Manager
+	Router  *chi.Mux
+}
+
+type ErrResponse struct {
+	HTTPStatusCode int    `json:"status"`
+	Message        string `json:"message"`
+}
+
+func (a *Api) StartTaskHandler(w http.ResponseWriter, r *http.Request) {
+	d := json.NewDecoder(r.Body)
+	d.DisallowUnknownFields()
+
+	te := task.TaskEvent{}
+
+	err := d.Decode(&te)
+
+	if err != nil {
+		msg := fmt.Sprintf("Error unmarshalling body: %v\n", err)
+		log.Printf(msg)
+		w.WriteHeader(400)
+		e := ErrResponse{
+			HTTPStatusCode: 400,
+			Message:        msg,
+		}
+		json.NewEncoder(w).Encode(e)
+		return
+	}
+
+	a.Manager.AddTask(te)
+	log.Printf("Added task %v\n", te.Task.ID)
+	w.WriteHeader(201)
+	json.NewEncoder(w).Encode(te)
+}
+
+func (a *Api) GetTasksHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader()
+	json.NewEncoder(w).Encode(a.Manager.GetTasks())
+}
+
+func (a *Api) StopTaskHandler(w http.ResponseWriter, r *http.Request) {
+	taskID := chi.URLParam(r, "taskID")
+	if taskID == "" {
+		log.Printf("No TaskID passed in request.\n")
+		w.WriteHeader(400)
+	}
+
+	tID, _ := uuid.Parse(taskID)
+
+	taskToStop, ok := a.Manager.TaskDB[tID]
+
+	if !ok {
+		log.Printf("No task with ID %v found", tID)
+		w.WriteHeader(404)
+	}
+
+	te := task.TaskEvent{
+		ID: uuid.New()
+		State: task.Completed,
+		Timestamp: time.Now(),
+	}
+
+	taskCopy := *taskToStop
+	taskCopy.State = task.Completed
+	te.Task = taskCopy
+	a.Manager.AddTask(te)
+
+	log.Printf("Added task event %v to stop task %v\n", te.ID, taskToStop.ID)
+	w.WriteHeader(204)
 
 }
+
+func (m *Manager) GetTasks() []*task.Task {
+	tasks := []*task.Task{}
+	for _, t := range m.TaskDB {
+		tasks = append(tasks, t)
+	}
+	return tasks
+}
+
+func (a *Api) initRouter() {
+	a.Router = chi.NewRouter()
+	a.Router.Route("/tasks", func(r chi.Router) {
+		r.Post("/", a.StartTaskHandler)
+		r.Get("/", a.GetTasksHandler)
+		r.Route("/{taskID}", func(r chi.Router){
+			r.Delete("/", a.StopTaskHandler)
+		})
+	})
+}
+
+func (a *Api) Start() {
+	a.initRouter()
+	http.ListenAndServe(fmt.Sprintf("%s:%d", a.Address, a.Port), a.Router)
+}
+
+
